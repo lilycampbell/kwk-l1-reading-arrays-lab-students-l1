847c5517307c1133873f5d79487f138d54ef03e6
lilycampbell <lilycatcampbell@gmail.com>
2018-06-05 22:24:36 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -11,34 +11,32 @@ def first_student_by_index
 STUDENT_NAMES[0]
 end
 
-puts first_student_by_index
+
 
 def fourth_student_by_index
  STUDENT_NAMES[3]
 end
 
-puts fourth_student_by_index
+
 
 def last_student_by_index
  STUDENT_NAMES[5]
 end
 
-puts last_student_by_index
+
 
 def first_student_by_method
  STUDENT_NAMES.first
 end
 
-puts first_student_by_method
+
 
 def last_student_by_method
  STUDENT_NAMES.last
 end
 
-puts last_student_by_method
+
 
 def first_second_and_third_students
  STUDENT_NAMES[1,2,3]
 end
-
-puts first_second_and_third_students
