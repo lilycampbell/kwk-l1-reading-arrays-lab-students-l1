9cb98e560b19b33498a26bb7460daba1669f4e52
lilycampbell <lilycatcampbell@gmail.com>
2018-06-05 22:08:51 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -14,8 +14,10 @@ end
 first_student_by_index
 
 def fourth_student_by_index
-  # Write a solution that returns the first student in the array student_names
+ puts STUDENT_NAMES[4]
 end
+
+fourth_student_by_index
 
 def last_student_by_index
   # Write a solution that returns the first student in the array student_names
