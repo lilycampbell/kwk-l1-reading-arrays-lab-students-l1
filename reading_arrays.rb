6f0c342e7bfd5d43bc1c8eeda046c4a890b37363
lilycampbell <lilycatcampbell@gmail.com>
2018-06-05 22:03:37 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -7,8 +7,8 @@ STUDENT_NAMES = [
   "Rihanna"
 ]
 
-def first_student_by_index
-  # Write a solution that returns the first student in the array student_names
+def
+puts STUDENT_NAMES[0]
 end
 
 def fourth_student_by_index
