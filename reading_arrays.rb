cd780541647a909f0b8befa78b619903d4d8b88f
lilycampbell <lilycatcampbell@gmail.com>
2018-06-05 22:23:18 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -29,16 +29,16 @@ def first_student_by_method
  STUDENT_NAMES.first
 end
 
-first_student_by_method
+puts first_student_by_method
 
 def last_student_by_method
  STUDENT_NAMES.last
 end
 
-last_student_by_method
+puts last_student_by_method
 
 def first_second_and_third_students
  STUDENT_NAMES[1,2,3]
 end
 
-first_second_and_third_students
+puts first_second_and_third_students
